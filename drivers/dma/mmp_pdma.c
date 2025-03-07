f8a50ceb02961da5ad5f7aeda28e04286c6860e5
Guodong Xu <guodong@riscstar.com>
2025-03-07 13:17:31 +0800

    dma: mmp_pdma: Add support for SpacemiT PDMA
    This patch adds the "spacemit,pdma-1.0" compatible string
    to the MMP PDMA driver's device tree match table. This allows
    the driver to properly recognize and support SpacemiT PDMA
    controllers when specified in the device tree.
    
    Signed-off-by: Guodong Xu <guodong@riscstar.com>

--- drivers/dma/mmp_pdma.c
+++ drivers/dma/mmp_pdma.c
@@ -996,6 +996,7 @@ static int mmp_pdma_chan_init(struct mmp_pdma_device *pdev, int idx, int irq)
 
 static const struct of_device_id mmp_pdma_dt_ids[] = {
 	{ .compatible = "marvell,pdma-1.0", },
+	{ .compatible = "spacemit,pdma-1.0", },
 	{}
 };
 MODULE_DEVICE_TABLE(of, mmp_pdma_dt_ids);
