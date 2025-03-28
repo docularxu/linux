415907de33dc5e89101d10e3a35d0553b1f68f99
Alex Elder <elder@riscstar.com>
2025-03-28 16:02:30 -0500

    clk: spacemit: make clocks optional
    There are some syscon devices that support both clocks and resets,
    but for now only their reset functionality is required.  Make
    defining clocks optional for a SpacemiT CCU, though at least one
    clock or at least one reset controller must be defined.
    
    Signed-off-by: Alex Elder <elder@riscstar.com>

--- drivers/clk/spacemit/ccu-k1.c
+++ drivers/clk/spacemit/ccu-k1.c
@@ -1369,11 +1369,16 @@ static const struct reset_control_ops spacemit_reset_control_ops = {
 };
 
 static int spacemit_ccu_register(struct device *dev,
-				 struct regmap *regmap, struct regmap *lock_regmap,
+				 struct regmap *regmap,
+				 struct regmap *lock_regmap,
 				 const struct spacemit_ccu_data *data)
 {
 	struct clk_hw_onecell_data *clk_data;
 	int i, ret;
+
+	/* Clocks are optional */
+	if (!data->clk_hws)
+		return 0;
 
 	clk_data = devm_kzalloc(dev, struct_size(clk_data, hws, data->clk_num),
 				GFP_KERNEL);
@@ -1486,6 +1491,7 @@ static int k1_ccu_probe(struct platform_device *pdev)
 	return 0;
 }
 
+/* Match data is required; its clk_hws or reset_data field must be non-null */
 static const struct of_device_id of_k1_ccu_match[] = {
 	{
 		.compatible	= "spacemit,k1-pll",
