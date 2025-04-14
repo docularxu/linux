071eeb37a1abfca5e75acf7001d4578912e2748e
Guodong Xu <guodong@riscstar.com>
2025-04-14 17:14:57 +0800

    FIXME: dma: k1 DCSR: add DCSR_EORIRQEN, DCSR_EORSTOPEN
    Signed-off-by: Guodong Xu <guodong@riscstar.com>

--- drivers/dma/mmp_pdma.c
+++ drivers/dma/mmp_pdma.c
@@ -1142,7 +1142,8 @@ static const struct mmp_pdma_config spacemit_k1_pdma_v1_config = {
 	.split_dma_addr_to_components = split_dma_addr_to_components_64_bits,
 	.combine_u64 = combine_u64_64_bits,
 	.read_iomem_combine_u64 = read_iomem_combine_u64_64_bits,
-	.dcsr_channel_run_bits = (DCSR_RUN | DCSR_LPAEEN),
+	.dcsr_channel_run_bits = (DCSR_RUN | DCSR_LPAEEN |
+				  DCSR_EORIRQEN | DCSR_EORSTOPEN),
 	.dma_mask = DMA_BIT_MASK(64),	/* support 64 bits address */
 };
 
